cb47d5cf46bc292c3bc02292a08c63ad9da70147
Javier Fontes <javierfontbas@gmail.com>
2025-04-13 22:49:34 -0400

    Version1.0

--- main.py
+++ main.py
@@ -10,19 +10,5 @@ from src.utils import json_generator as jg
 from src.evaluators import evaluator, results_evaluator
 
 
-"""with open('data/processed/expanded_story.json', 'r',encoding="utf-8" ) as archivo:
-        dataset = json.load(archivo)  # Convierte el contenido del archivo en un diccionario
-
-for i in range(50,100):
-        story=dataset[f"story_{str(i+1).zfill(3)}"]["original_text"]["text"]
-        story=qna.qna_expander(i,story)
-
-        dataset[f"story_{str(i+1).zfill(3)}"]["expanded_stories"].append({"qna_model":[{"story":story,"answer":None}]})
-
-        with open("data/processed/expanded_story.json","w",encoding="utf-8") as f:
-                        json.dump(dataset, f, indent=4,ensure_ascii=False)
-        print(f"story_{str(i+1).zfill(3)}")"""
-
-
 results_evaluator.results_evaluation()
 #evaluator.evaluator()
